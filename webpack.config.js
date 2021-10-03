e4a5553fdd3bec131df7a79b6de6e349c1f074f3
gvacc <ivadenlol@gmail.com>
2021-10-03 18:20:10 +0500

    окружение

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/elven-scout.js'
-  },
-  devServer: {
-    contentBase: './dist'
-  },  
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  }
+	mode: "development",
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/elven-scout.js'
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		compress: false,
+		port: 9000,
+	},  
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+		{
+			test: /\.m?js$/,
+			exclude: /(node_modules|bower_components)/,
+			use: {
+			loader: 'babel-loader',
+			options: {
+				presets: ['@babel/preset-env']
+			}
+			}
+		}
+		]
+	}
 };
