3763cfa636c90c1c2e0de07a6a3a90c13d9ded9e
gvacc <ivadenlol@gmail.com>
2021-10-04 14:42:58 +0500

    Добавлена загрузка изображений, добавлена сцена меню

--- src/Game.js
+++ src/Game.js
@@ -1,22 +1,40 @@
 import { Screen } from "./Screen"
 import { Loading } from "./scenes/Loading"
+import { Menu } from "./scenes/Menu"
+import { Scene } from "./Scene"
 
 export class Game {
-	constructor() {
-		this.screen = new Screen(500, 500)
+	constructor({width = 500, height = 500} = {}) {
+		this.screen = new Screen(width, height)
+		this.screen.loadImages({
+			orc: 'img/orc.png',
+			player: 'img/player.png',
+			title: 'img/title.jpg',
+			tiles: 'img/tiles.png'
+		})
 		this.scenes = {
-			loading: new Loading(this)
+			loading: new Loading(this),
+			menu: new Menu(this)
 		}
 		this.currentScene = this.scenes.loading
 		this.currentScene.init()
 	}
 
+	changeScene(status) {
+		switch (status) {
+			case Scene.LOADED:
+                return this.scenes.menu;
+			default:
+				return this.scenes.menu
+		}
+	}
+
 	frame(time) {
-		if(!this.currentScene.isActive) {
-			this.currentScene = this.scenes[this.currentScene.nextScene]
+		if(!this.currentScene.status != Scene.WORKING) {
+			this.currentScene = this.changeScene(this.currentScene.status)
 			this.currentScene.init()
 		}
-		this.currentScene.render()
+		this.currentScene.render(time)
 		requestAnimationFrame(time => this.frame(time))
 	}
 
--- src/Screen.js
+++ src/Screen.js
@@ -1,26 +1,46 @@
+import { ImageLoader } from "./ImageLoader"
+
 export class Screen {
 	constructor(width, height) {
 		this.width = width || 500
 		this.height = height || 500
-		this.canvas = this.createCanvas()
-		this.canvas.width = width
-		this.canvas.height = height
+		this.canvas = this.createCanvas(width, height)
 		this.context = this.canvas.getContext('2d')
+		this.images = {}
+		this.isImagesLoaded = false
 	}
 
-	createCanvas() {
+	createCanvas(width, height) {
 		const canvasElements = document.querySelectorAll('canvas')
-		if(canvasElements.length > 0) {
-			return canvasElements[0]
-		}
-		const canvas = document.createElement('canvas')
+		const canvas = canvasElements[0] || document.createElement('canvas')
+		canvas.width = width
+		canvas.height = height
 		document.body.appendChild(canvas)
 		return canvas
 	}
+
+	loadImages(imageFiles) {
+        const loader = new ImageLoader(imageFiles);
+		loader.load().then((names) => {
+            this.images = Object.assign(this.images, loader.images)
+            this.isImagesLoaded = true
+        })
+    }
 
 	fill(color) {
 		console.log('fill')
 		this.context.fillStyle = color
 		this.context.fillRect(0,0, this.width, this.height)
 	}
+
+	print(x, y, text) {
+        this.context.fillStyle = "#FFFFFF";
+        this.context.font = "22px Georgia";
+        this.context.fillText(text, x, y);
+    }
+
+	drawImage(x, y, imageName) {
+		console.log(this.images[imageName])
+        this.context.drawImage(this.images[imageName], x, y);
+    }
 }
